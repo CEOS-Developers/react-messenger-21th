e2b4026e48de7c239edc7b855f2375b5b1b8033c
leejuhi <anim0203@naver.com>
2025-03-24 23:26:54 +0900

    chore: 기본 세팅

---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    :root{
+        back-ground-color: #f5f5f5;
+    }
+    body {
+        margin: 0;
+        padding: 0;
+        font-family: 'Roboto', sans-serif;
+    }
+`;
+
+export default GlobalStyles;
