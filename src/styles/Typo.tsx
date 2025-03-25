dde81d80884055ad7610fed96e32a66d251202f9
Yeongseo Kim <1903020@ewhain.net>
2025-03-25 13:53:02 +0900

    feat: 컬러 시스템, 타이포 시스템 설정

---
+++ src/styles/Typo.tsx
@@ -0,0 +1,64 @@
+import { css } from 'styled-components'
+
+interface ExecutionContext {
+  $isM: boolean
+  $isR: boolean
+}
+
+const fontWeight = {
+  Bold: '700',
+  Semibold: '600',
+  Medium: '500',
+  Regular: '400',
+}
+
+const Headline1 = css`
+  font-weight: ${fontWeight.Bold};
+  font-size: 28px;
+  line-height: 155%;
+  letter-spacing: calc(-0.1% * 28px);
+`
+
+const Headline2 = css`
+  font-weight: ${fontWeight.Bold};
+  font-size: 24px;
+  line-height: 155%;
+  letter-spacing: calc(-0.1% * 24px);
+`
+
+const Headline3 = css`
+  font-weight: ${fontWeight.Semibold};
+  font-size: 20px;
+  line-height: 155%;
+  letter-spacing: calc(-0.1% * 20px);
+`
+const Subhead = css<ExecutionContext>`
+  font-weight: ${({ $isM }) => ($isM ? fontWeight.Medium : fontWeight.Bold)};
+  font-size: 18px;
+  line-height: 150%;
+  letter-spacing: calc(-0.1% * 18px);
+`
+
+const Body_1 = css<ExecutionContext>`
+  font-weight: ${({ $isM }) => ($isM ? fontWeight.Medium : fontWeight.Bold)};
+  font-size: 16px;
+  line-height: 150%;
+  letter-spacing: calc(-0.1% * 16px);
+`
+
+const Body_2 = css<ExecutionContext>`
+  font-weight: ${({ $isM, $isR }) =>
+    $isM ? fontWeight.Medium : $isR ? fontWeight.Regular : fontWeight.Bold};
+  font-size: 14px;
+  line-height: 150%;
+  letter-spacing: calc(-0.1% * 14px);
+`
+
+const Caption = css<ExecutionContext>`
+  font-weight: ${({ $isM }) => ($isM ? fontWeight.Medium : fontWeight.Bold)};
+  font-size: 12px;
+  line-height: 140%;
+  letter-spacing: calc(-0.1% * 12px);
+`
+
+export { Headline1, Headline2, Headline3, Subhead, Body_1, Body_2, Caption }
---
+++ src/utils/theme.ts
@@ -0,0 +1,26 @@
+const colors = {
+  white: '#FFFFFF',
+  gray01: '#F8F9FB',
+  gray02: '#F3F5FA',
+  gray03: '#EEEFF2',
+  gray04: '#DDDFE5',
+  gray05: '#CACDD4',
+  gray06: '#B1B4BC',
+  gray07: '#9C9EA5',
+  gray08: '#7E8085',
+  gray09: '#717377',
+  gray10: '#5E6064',
+  gray11: '#505154',
+  gray12: '#3C3E41',
+  gray13: '#2C2D30',
+  gray14: '#181A1B',
+  black: '#000000',
+  mainSkyblue: 'linear-gradient( #FFFFFF, #7EDFFF)',
+  mainOrange: 'linear-gradient( #FFFFFF, #FEB985)',
+  mainPurple: 'linear-gradient( #FFFFFF, #D9B9FA)',
+  mainLime: 'linear-gradient( #FFFFFF, #EAFB88)',
+  chatBackground: 'linear-gradient( #A6D0FF 29%, #FFFFFF 69%, #8BC2FF)',
+  profileBackground: 'linear-gradient( #FFFFFF, #92C6FF)',
+}
+
+export { colors }
