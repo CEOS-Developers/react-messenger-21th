d8915b1160aec3255c8391aa9453548664fee5a3
Yeongseo Kim <1903020@ewhain.net>
2025-03-25 13:55:36 +0900

    design: variable pretendard font 및  전역 스타일 설정

---
+++ src/styles/GlobalStyle.jsx
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+@font-face {
+  font-family: 'Pretendard Variable';
+  font-weight: 45 920;
+  font-style: normal;
+  font-display: swap;
+  src: url('../assets/font/PretendardVariable.woff2') format('woff2-variations');
+}
+
+body, html {
+  margin: 0;
+  height: 100%;
+}
+
+a {
+  text-decoration: none;
+  color: inherit;
+}
+`
+
+export default GlobalStyle
