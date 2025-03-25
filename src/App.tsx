fb1a21c3a8fd05d52a1dd58f54646c983c63c8bd
Yeongseo Kim <1903020@ewhain.net>
2025-03-25 15:35:26 +0900

    feat: react-router 추가 및 Router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import './App.css'
+import { Route, Routes } from 'react-router'
+
+/** styles */
+import { ThemeProvider } from 'styled-components'
+import theme from './styles/theme.ts'
+import GlobalStyle from './styles/GlobalStyle.ts'
+
+/** pages */
+import ChatRoom from './pages/ChatRoom.tsx'
 
 function App() {
-  return <></>
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Routes>
+          <Route path="/" element={<ChatRoom />} />
+        </Routes>
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default App
