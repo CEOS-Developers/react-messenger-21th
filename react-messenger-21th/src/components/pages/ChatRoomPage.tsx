30061593fac4fb81c3b2f6db15564e2b080dcdda
only1Ksy <winterksy@gmail.com>
2025-03-26 13:50:15 +0900

    feat: 채팅방 내 버튼 추가

--- react-messenger-21th/src/components/pages/ChatRoomPage.tsx
+++ react-messenger-21th/src/components/pages/ChatRoomPage.tsx
@@ -2,19 +2,35 @@ import { useState } from 'react';
 import * as s from '../styles/ChatRoomPageStyles';
 import ChatBoard from '../chatComponents/ChatBoard.tsx';
 import ChatInput from '../chatComponents/ChatInput.tsx';
+import styled from 'styled-components';
 
 const ChatRoomPage: React.FC = () => {
   return (
     <s.ChatContainer>
-      <s.UpperBarContainer></s.UpperBarContainer>
+      <s.UpperBarContainer>
+        <PrevButton />
+        <CurrentUsers />
+        <SearchButton />
+        <MenuButton />
+      </s.UpperBarContainer>
       <s.ChatContentsContainer>
         <ChatBoard />
       </s.ChatContentsContainer>
       <s.BottomBarContainer>
+        <PlusButton />
         <ChatInput />
       </s.BottomBarContainer>
     </s.ChatContainer>
   );
 };
+
+const PrevButton = styled.button``;
+const CurrentUsers = styled.span``;
+
+const SearchButton = styled.button``;
+
+const MenuButton = styled.button``;
+
+const PlusButton = styled.button``;
 
 export default ChatRoomPage;
