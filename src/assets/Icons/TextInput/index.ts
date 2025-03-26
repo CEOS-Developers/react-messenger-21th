a0fe7617b48736118cf918a851fe144d62fd415e
Yeongseo Kim <1903020@ewhain.net>
2025-03-26 13:34:08 +0900

    chore: svg 아이콘 및 svg 처리기 추가

---
+++ src/assets/Icons/TextInput/index.ts
@@ -0,0 +1,6 @@
+import AddIcon from './add.svg?react'
+import FaceIcon from './face.svg?react'
+import MicrophoneIcon from './microphone.svg?react'
+import SendIcon from './send.svg?react'
+
+export { AddIcon, FaceIcon, MicrophoneIcon, SendIcon }
