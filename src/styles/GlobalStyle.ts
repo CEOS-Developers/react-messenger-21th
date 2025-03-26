b0d4aaeab64d2c1d5118def8c4f9193fe27781fb
2025314242 <donguk.kwon@yonsei.ac.kr>
2025-03-26 13:26:52 +0900

    design: set background color
    set background color to black

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -5,6 +5,7 @@ const GlobalStyle = createGlobalStyle`
   html, body {
     font-family: 'Pretendard Variable', -apple-system, BlinkMacSystemFont,
       'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    background-color: #000000;
   }
 `;
 
