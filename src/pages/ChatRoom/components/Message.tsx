d9ab6e1c112b82b4a5a91a355b4aac8b06587af5
young <azerosong@gmail.com>
2025-03-26 17:05:16 +0900

    fix: 텍스트가 길 때 자동 개행

--- src/pages/ChatRoom/components/Message.tsx
+++ src/pages/ChatRoom/components/Message.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from 'react';
 export default function Message({ isReceived, children }: { isReceived: boolean; children: ReactNode }) {
 	return (
 		<div
-			className={`relative px-2.5 py-2 rounded-lg body2-regular-lh150
+			className={`relative px-2.5 py-2 rounded-lg body2-regular-lh150 whitespace-pre-wrap break-words
         ${isReceived ? 'bg-black-000 max-w-[13.875rem]' : 'bg-main-200 max-w-[16.625rem]'}`}
 		>
 			{children}
