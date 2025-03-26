f29751a6ccd657787d2020a25d2c9c43c61319b0
only1Ksy <winterksy@gmail.com>
2025-03-27 01:37:49 +0900

    style: 아이콘 배치, svg 컴포넌트 환경설정

--- react-messenger-21th/src/components/pages/ChatRoomPage.tsx
+++ react-messenger-21th/src/components/pages/ChatRoomPage.tsx
@@ -3,6 +3,9 @@ import * as s from '../styles/ChatRoomPageStyles';
 import ChatBoard from '../chatComponents/ChatBoard.tsx';
 import ChatInput from '../chatComponents/ChatInput.tsx';
 import styled from 'styled-components';
+import SearchButtonIcon from '../../assets/icons/SearchUpperBar.svg?react';
+import MenuButtonIcon from '../../assets/icons/Hamburger.svg?react';
+import PlusButtonIcon from '../../assets/icons/PlusNotSelected.svg?react';
 
 const ChatRoomPage: React.FC = () => {
   return (
@@ -10,14 +13,20 @@ const ChatRoomPage: React.FC = () => {
       <s.UpperBarContainer>
         <PrevButton />
         <CurrentUsers />
-        <SearchButton />
-        <MenuButton />
+        <SearchButton>
+          <SearchButtonIcon width="24px" height="24px" />
+        </SearchButton>
+        <MenuButton>
+          <MenuButtonIcon width="24px" height="24px" />
+        </MenuButton>
       </s.UpperBarContainer>
       <s.ChatContentsContainer>
         <ChatBoard />
       </s.ChatContentsContainer>
       <s.BottomBarContainer>
-        <PlusButton />
+        <PlusButton>
+          <PlusButtonIcon width="24px" height="24px" />
+        </PlusButton>
         <ChatInput />
       </s.BottomBarContainer>
     </s.ChatContainer>
--- react-messenger-21th/vite.config.ts
+++ react-messenger-21th/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        icon: true, // ✅ SVG 아이콘처럼 사용 가능하도록 설정
+      },
+    }),
+  ],
+});
