2b45f56b1dfccf582ffc5f81969e6526a1980f49
한서정 / Seojung Han <sjhan0814@gmail.com>
2025-03-27 13:48:56 +0900

    chore. 라우팅 설정

--- messenger/src/App.tsx
+++ messenger/src/App.tsx
@@ -1,5 +1,11 @@
+import { Routes, Route } from "react-router-dom";
+import ChatList from "./pages/ChatList";
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<ChatList />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
