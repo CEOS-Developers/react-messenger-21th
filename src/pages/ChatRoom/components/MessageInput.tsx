86e58fcab1df71e753d2bab03020f51337ccb398
young <azerosong@gmail.com>
2025-03-27 17:48:17 +0900

    feat: 메시지 입력 영역 높이 조절 기능 추가

--- src/pages/ChatRoom/components/MessageInput.tsx
+++ src/pages/ChatRoom/components/MessageInput.tsx
@@ -1,18 +1,23 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useRef, useState } from 'react';
 
 import Plus from '@/assets/icons/plus.svg?react';
 import Send from '@/assets/icons/send.svg?react';
 
 export default function MessageInput({ onSubmit }: { onSubmit: (message: string) => void }) {
 	const [message, setMessage] = useState('');
+	const textareaRef = useRef<HTMLTextAreaElement | null>(null);
 
-	const handleMessageChange = (e: ChangeEvent<HTMLInputElement>) => {
+	const handleMessageChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
 		setMessage(e.target.value);
+
+		// 높이 자동 조절
+		if (textareaRef.current) {
+			textareaRef.current.style.height = 'auto';
+			textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+		}
 	};
 
 	const handleSendButtonClick = () => {
-		if (!message.trim()) return;
-
 		onSubmit(message);
 		setMessage('');
 	};
@@ -27,18 +32,21 @@ export default function MessageInput({ onSubmit }: { onSubmit: (message: string)
 	};
 
 	return (
-		<div className="absolute bottom-0 w-full p-3 border-t border-black-200 flex gap-2 items-center bg-black-000">
+		<div className="w-full p-3 pb-[2.625rem] border-t border-black-200 flex gap-2 items-center bg-black-000">
 			<button className="w-8 h-8 bg-black-100 rounded-full">
 				<Plus className="m-auto text-black-400" />
 			</button>
 
-			<input
-				type="text"
+			<textarea
+				rows={1}
+				ref={textareaRef}
 				value={message}
 				onChange={handleMessageChange}
 				onKeyDown={handleEnterKeyDown}
 				placeholder="메시지 보내기"
-				className="px-2.5 h-10 flex-grow bg-black-100 rounded-full outline-0 body1-medium message-input"
+				className="px-5 py-2.5 max-h-[5.75rem] flex-grow flex items-center justify-center
+					bg-black-100 rounded-[1.25rem] outline-0 body1-medium-lh150 message-input resize-none"
+				style={{ scrollbarWidth: 'none' }}
 			/>
 
 			<button
