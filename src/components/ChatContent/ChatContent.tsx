9af87a9e3d7236d83e98597d584cd298ce4998a8
Yeongseo Kim <1903020@ewhain.net>
2025-03-27 17:25:45 +0900

    feat: user 불러오기, member 표현, 이름 클릭 시 member 교체 구현

--- src/components/ChatContent/ChatContent.tsx
+++ src/components/ChatContent/ChatContent.tsx
@@ -1,31 +1,59 @@
+import { useEffect, useState } from 'react'
 import { Content } from '../common/Common.Styled'
 import ContentHeader from '../common/ContentHeader'
+import ChatTitle from './ChatTitle'
 import TextInput from './TextInput'
 import { ChatRoomIcon } from '../../assets/Icons/Header'
-import ChatTitle from './ChatTitle'
+import { userData } from '../../assets/data/user.json'
+import { chatRoomData } from '../../assets/data/chatRoom.json'
+import { User } from '../../interface/User'
+import { ChatRoom } from '../../interface/ChatRoom'
 
-const MY_USER_ID = 0
-const mockData = {
-  //chatList
-  roomName: '이채현',
-  member: [0, 1], //user.id
-}
-const mockData2 = {
-  roomName: 'CEOS 21기 공지방',
-  member: Array.from({ length: 50 }, (_, i) => i),
-}
+const ROOM_ID = '550e8400-e29b-41d4-a716-446655440000'
 
 const ChatContent = () => {
+  const [userId, setUserId] = useState(1)
+  const [user, setUser] = useState<User | null>(null)
+  const [room, setRoom] = useState<ChatRoom | null>(null)
   const s = { Content }
-  // filter: 나를 제외한 멤버를 props로
-  const titleData = {
-    ...mockData2,
-    member: [...mockData2.member].filter((id) => id !== MY_USER_ID),
+
+  useEffect(() => {
+    const curUser = userData.find((user) => user.id === userId)
+    setUser(curUser || null)
+
+    const curRoom = chatRoomData.find((room) => room.chatRoomId === ROOM_ID)
+    setRoom(curRoom || null)
+  }, [])
+
+  useEffect(() => {
+    const curUser = userData.find((user) => user.id === userId)
+    setUser(curUser)
+  }, [userId])
+
+  const getMember = () => {
+    const memberId = room.member.filter((id: number) => id !== userId)
+    return memberId.map(
+      (id: number) => userData.find((user) => user.id === id)?.name
+    )
   }
+
+  const handleClickMemberName = () => {
+    const memberId = room.member.filter((id: number) => id !== userId)
+    setUserId(memberId[0])
+  }
+
+  if (!room || !user) return <div>Loading...</div>
+
   return (
     <s.Content>
       <ContentHeader
-        leftChild={<ChatTitle {...titleData} />}
+        leftChild={
+          <ChatTitle
+            roomName={room.roomName}
+            member={getMember()}
+            handleClickMemberName={handleClickMemberName}
+          />
+        }
         rightChild={<ChatRoomIcon />}
       />
       <div>말풍선1, 말풍선2, ...</div>
--- src/components/ChatContent/ChatTitle.tsx
+++ src/components/ChatContent/ChatTitle.tsx
@@ -1,12 +1,13 @@
 import Back from '../../assets/Icons/Header/back.svg?react'
 import * as s from './ChatTitle.Styled'
 
-const ChatTitle = ({ roomName, member }) => {
-  //ChatMember.length > 1 ? member.length + 1 표시
+const ChatTitle = ({ roomName, member, handleClickMemberName }) => {
   return (
     <s.ChatTitleContainer>
       <Back />
-      <s.ChatName>{roomName}</s.ChatName>
+      <s.ChatName onClick={handleClickMemberName}>
+        {roomName ?? member.join(', ')}
+      </s.ChatName>
       <s.MemberNum>{member.length > 1 ? member.length + 1 : ''}</s.MemberNum>
     </s.ChatTitleContainer>
   )
