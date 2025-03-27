e6c3c715badd12b780407106589dca7984ec6959
young <azerosong@gmail.com>
2025-03-27 17:49:05 +0900

    feat: 상단 status bar 추가

--- src/components/Frame.tsx
+++ src/components/Frame.tsx
@@ -1,11 +1,25 @@
 import { ReactNode } from 'react';
 
+import Cellular from '@/assets/icons/cellular.svg?react';
+import Wifi from '@/assets/icons/wifi.svg?react';
+import Battery from '@/assets/icons/battery.svg?react';
+
 export default function Frame({ children }: { children: ReactNode }) {
+	const bgColor = 'bg-black-200';
+
 	return (
 		<div
-			className="w-[23.4375rem] h-[45.875rem] flex flex-col m-auto
+			className="w-[375px] flex flex-col m-auto
 				shadow-[0_1px_10px_0_rgba(0,0,0,0.2)]"
 		>
+			<div className={`pt-[1.3125rem] pb-[0.3125rem] flex-grow flex justify-between ${bgColor}`}>
+				<div className="ml-[3.0781rem]">9:41</div>
+				<div className="mr-[1.8125rem] flex gap-[0.4375rem]">
+					<Cellular />
+					<Wifi />
+					<Battery />
+				</div>
+			</div>
 			{children}
 		</div>
 	);
