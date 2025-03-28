2a234f1066cd7f21a43e83e63d98be052722bd81
seoyeon5117 <seoyun020207@gmail.com>
2025-03-28 20:37:45 +0900

    chore: 너비 조정

--- src/components/layout/GlobalLayout.tsx
+++ src/components/layout/GlobalLayout.tsx
@@ -1,6 +1,6 @@
 const GlobalLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='flex flex-col h-screen min-w-[20rem] max-w-[40rem] bg-white md:w-[28rem]'>
+    <div className='flex flex-col h-screen min-w-[20rem] w-screen max-w-[40rem] bg-white md:w-[28rem]'>
       <main className='flex relative flex-1 flex-col overflow-y-auto'>
         {children}
       </main>
