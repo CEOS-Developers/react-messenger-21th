b31c50015a2c8935e0a99fb99faf5f4f119e06c9
Yeongseo Kim <1903020@ewhain.net>
2025-03-30 13:41:27 +0900

    fix: safari Invalid Date 오류 수정

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,6 +1,11 @@
+/* YYYY년 M월 D일 D요일 */
 const formatDate = (date: string): string => {
   const dateObj = new Date(date)
-  const formattedDate = dateObj.toLocaleDateString('ko-KR', {
+  const validDateObj = isNaN(dateObj.getTime())
+    ? new Date(date.replace(/-/g, '/')) // Safari Invalid Date 오류 처리
+    : dateObj
+
+  const formattedDate = validDateObj.toLocaleDateString('ko-KR', {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
@@ -9,6 +14,7 @@ const formatDate = (date: string): string => {
   return formattedDate
 }
 
+/* 오전or오후 H시 M분 */
 const formatTime = (time: number): string => {
   const dateObj = new Date(time)
   const formattedTime = dateObj.toLocaleString('ko-KR', {
@@ -19,13 +25,13 @@ const formatTime = (time: number): string => {
   return formattedTime
 }
 
+/* YYYY-MM-DD */
 const formatDateForData = (dateObj: Date): string => {
-  const formattedDate = dateObj.toLocaleDateString('ko-KR', {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-  })
-  return formattedDate.replace(/\./g, '-')
+  const year = dateObj.getFullYear()
+  const mm = String(dateObj.getMonth() + 1).padStart(2, '0')
+  const dd = String(dateObj.getDate()).padStart(2, '0')
+
+  return `${year}-${mm}-${dd}`
 }
 
 export { formatDate, formatTime, formatDateForData }
