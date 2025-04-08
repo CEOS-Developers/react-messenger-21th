d023435343e23f7f8f701439b42964011b0be0cc
young <azerosong@gmail.com>
2025-04-08 20:29:13 +0900

    feat: 채팅 리스트 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import ChatRoom from './pages/ChatRoom';
 import Frame from './components/Frame';
+import ChatList from './pages/ChatList';
+import ChatRoom from './pages/ChatRoom';
 
 import '@/styles/global.css';
 
 function App() {
 	return (
-		<Frame>
-			<BrowserRouter>
+		<BrowserRouter>
+			<Frame>
 				<Routes>
+					<Route path="/chatlist" element={<ChatList />} />
 					<Route path="/chatroom/:chatRoomId" element={<ChatRoom />} />
 				</Routes>
-			</BrowserRouter>
-		</Frame>
+			</Frame>
+		</BrowserRouter>
 	);
 }
 
