3f0cb822284bdfca14f879c07a29988bebcf828d
young <azerosong@gmail.com>
2025-04-08 20:28:01 +0900

    style: flex-grow -> grow로 수정

--- src/components/Frame.tsx
+++ src/components/Frame.tsx
@@ -1,18 +1,21 @@
 import { ReactNode } from 'react';
+import { useLocation } from 'react-router-dom';
 
 import Cellular from '@/assets/icons/cellular.svg?react';
 import Wifi from '@/assets/icons/wifi.svg?react';
 import Battery from '@/assets/icons/battery.svg?react';
 
 export default function Frame({ children }: { children: ReactNode }) {
-	const bgColor = 'bg-black-200';
+	const location = useLocation();
+	const pathname = location.pathname;
+	const bgColor = pathname === '/chatlist' ? 'bg-black-000' : 'bg-black-200';
 
 	return (
 		<div
 			className="w-[375px] flex flex-col m-auto rounded-4xl
 				shadow-[0_1px_10px_0_rgba(0,0,0,0.2)]"
 		>
-			<div className={`pt-[1.3125rem] pb-[0.3125rem] flex-grow flex justify-between rounded-t-4xl ${bgColor}`}>
+			<div className={`pt-[1.3125rem] pb-[0.3125rem] grow flex justify-between rounded-t-4xl ${bgColor}`}>
 				<div
 					className="ml-[3.0781rem]"
 					style={{ fontFamily: 'SF Pro', fontSize: '17px', fontStyle: 'normal', fontWeight: 600, lineHeight: '22px' }}
--- src/pages/ChatRoom/index.tsx
+++ src/pages/ChatRoom/index.tsx
@@ -55,7 +55,7 @@ export default function ChatRoom() {
 	};
 
 	return (
-		<div className="relative flex-grow flex flex-col bg-black-200 rounded-b-4xl">
+		<div className="relative grow flex flex-col bg-black-200 rounded-b-4xl">
 			<TopBar content={otherUserContent} onClickContent={handleTopBarContentClick} />
 			<MessageContainer messagesByUsers={messagesByUsers} currentUserId={currentUserId} />
 			<MessageInput onSubmit={handleMessageSubmit} />
