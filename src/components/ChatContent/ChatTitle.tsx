42f9e5404beec143af9e6c1f444e425bfb2b6ae7
Yeongseo Kim <1903020@ewhain.net>
2025-04-13 00:35:39 +0900

    feat: 채팅방 이름 클릭 시 유저 변경 이벤트 삭제
    멤버 리스트에서 선택할 수 있음

--- src/components/ChatContent/ChatTitle.tsx
+++ src/components/ChatContent/ChatTitle.tsx
@@ -5,30 +5,19 @@ import { EventIcon } from '../common/Common.Styled'
 import Back from '../../assets/Icons/Header/back.svg?react'
 
 import getRoomName from '../../utils/getRoomName'
-import setUserAndChatRoom from '../../utils/setUserAndChatRoom'
 
 const ChatTitle = ({ memberIds }: { memberIds: number[] }) => {
   const nav = useNavigate()
   const { roomName, memberCount } = useLocation().state ?? {}
   const isGroupChat = memberCount > 2
 
-  /* ChatTitle의 멤버의 이름을 클릭하면 사용자가 바뀌는 이벤트 */
-  const handleClickRoomName = () => {
-    if (!isGroupChat) return setUserAndChatRoom(memberIds[0])
-    nav('./members')
-  }
-
   return (
     <s.ChatTitleContainer>
-      <EventIcon onClick={() => nav(-1)}>
+      <EventIcon onClick={() => nav('/chatList')}>
         <Back width={24} />
       </EventIcon>
-      <EventIcon>
-        <s.ChatName onClick={handleClickRoomName}>
-          {roomName || getRoomName(memberIds)}
-        </s.ChatName>
-        <s.MemberNum>{isGroupChat ? memberCount : null}</s.MemberNum>
-      </EventIcon>
+      <s.ChatName>{roomName || getRoomName(memberIds)}</s.ChatName>
+      <s.MemberNum>{isGroupChat ? memberCount : null}</s.MemberNum>
     </s.ChatTitleContainer>
   )
 }
