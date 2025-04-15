b815ee64d87768fdf98e6c1b467bd90f10169d52
2025314242 <donguk.kwon@yonsei.ac.kr>
2025-04-15 19:10:42 +0900

    fix: remove unused properties

--- src/components/chat/ChatList/ChatList.Styled.ts
+++ src/components/chat/ChatList/ChatList.Styled.ts
@@ -1,10 +1,7 @@
 import styled from 'styled-components';
 
 const ChatListWrapper = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
 `;
