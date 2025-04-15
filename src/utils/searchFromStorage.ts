e243fc9ae2a66694226088579059f02669a0e6cd
2025314242 <donguk.kwon@yonsei.ac.kr>
2025-04-15 21:27:09 +0900

    fix: no input -> return all corpus

--- src/utils/searchFromStorage.ts
+++ src/utils/searchFromStorage.ts
@@ -1,7 +1,7 @@
 import Hangul from 'hangul-js';
 
 function searchFromStorage(searchValue: string, corpus: string[]): number[] {
-  if (!searchValue) return [];
+  if (!searchValue) return corpus.map((_, i) => i);
 
   return corpus.reduce<number[]>((acc, text, idx) => {
     if (Hangul.search(text.toLowerCase(), searchValue.toLowerCase()) > -1) {
