c598da8ccd5b16fc01f19a0db552f2261d3cd599
Yeongseo Kim <1903020@ewhain.net>
2025-04-15 21:02:37 +0900

    refactor(ChatListPage): styled-components->tailwind, 레이아웃 조정

--- src/components/ChatListContent/ChatListContent.tsx
+++ src/components/ChatListContent/ChatListContent.tsx
@@ -1,18 +1,26 @@
 import { ChatListIcon } from '../../assets/Icons/AppBar'
-import * as s from './ChatListContent.Styled'
 import ChatListViewer from './ChatListViewer'
+import HomeBar from '@/components/Common/HomeBar'
+import NavigationBar from '@/components/Common/NavigationBar'
+import { NavRole } from '@/utils/constants'
 
 const ChatListContent = () => {
   return (
-    <s.CLContent>
+    <div className="container">
       <div className="pt-1">
         <div className="app-bar app-bar--62">
-          <s.Title>대화</s.Title>
+          <p className="font-Headline2">대화</p>
           <ChatListIcon />
         </div>
       </div>
+
       <ChatListViewer />
-    </s.CLContent>
+
+      <div className="border-gray08 bg-white">
+        <NavigationBar select={NavRole.CHAT_LIST} />
+        <HomeBar />
+      </div>
+    </div>
   )
 }
 
--- src/pages/ChatList.tsx
+++ src/pages/ChatList.tsx
@@ -1,18 +1,11 @@
 import StatusBar from '@/components/Common/StatusBar'
-import HomeBar from '@/components/Common/HomeBar'
-import NavigationBar from '@/components/Common/NavigationBar'
 import ChatListContent from '@/components/ChatListContent/ChatListContent'
-import { NavRole } from '@/utils/constants'
 
 const ChatList = () => {
   return (
-    <div>
+    <div className="wrapper">
       <StatusBar />
       <ChatListContent />
-      <div className="border-gray08 bg-white">
-        <NavigationBar select={NavRole.CHAT_LIST} />
-        <HomeBar />
-      </div>
     </div>
   )
 }
