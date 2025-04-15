c534cd2358715f214d0c32694178c49a01f70d1e
2025314242 <donguk.kwon@yonsei.ac.kr>
2025-04-15 19:09:37 +0900

    fix: minor change ... ?

--- src/components/home/HomeHeader/HomeHeader.tsx
+++ src/components/home/HomeHeader/HomeHeader.tsx
@@ -4,7 +4,7 @@ import * as Icons from '@/assets/icons/home';
 function HomeHeader() {
   return (
     <S.HomeHeaderWrapper className="bg-grayscale-07-white">
-      <Icons.AddPerson className="w-[24px] h-[24px]" />
+      <Icons.AddUser className="w-[24px] h-[24px]" />
       <Icons.Settings className="w-[24px] h-[24px]" />
       <Icons.Bell className="w-[24px] h-[24px]" />
     </S.HomeHeaderWrapper>
