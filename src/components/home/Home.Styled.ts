e40b3781180692ad3e5477b77772ee7de00c4d6d
2025314242 <donguk.kwon@yonsei.ac.kr>
2025-04-15 19:10:20 +0900

    feat: add Home component

--- src/components/home/Home.Styled.ts
+++ src/components/home/Home.Styled.ts
@@ -1,5 +1,21 @@
 import styled from 'styled-components';
 
-const HomeWrapper = styled.div``;
+const HomeWrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
 
-export { HomeWrapper };
+const UserGroup = styled.div`
+  padding-bottom: 83px;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  overflow-y: scroll;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export { HomeWrapper, UserGroup };
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,7 +1,37 @@
 import * as S from './Home.Styled';
+import { useMyId, useUserList } from '@/contexts/localStorage';
+import HomeHeader from './HomeHeader/HomeHeader';
+import UserMe from './UserMe/UserMe';
+import UserList from './UserList/UserList';
+import { filterFavoriteUserList } from './Home.utils';
 
 function Home() {
-  return <S.HomeWrapper></S.HomeWrapper>;
+  const { myId } = useMyId();
+  const { userList, onToggleFavorite } = useUserList();
+
+  // Must render after all data are loaded
+  if (!myId || !userList[myId]) return null;
+
+  const otherUserList = { ...userList };
+  delete otherUserList[myId];
+
+  const filteredOtherUserList = filterFavoriteUserList(otherUserList);
+
+  return (
+    <S.HomeWrapper>
+      <HomeHeader />
+      <S.UserGroup>
+        <UserMe user={userList[myId]} />
+        <UserList
+          userList={filteredOtherUserList}
+          isFavorite={true}
+          isSearch={false}
+          onToggleFavorite={onToggleFavorite}
+        />
+        <UserList userList={otherUserList} isFavorite={false} isSearch={true} onToggleFavorite={onToggleFavorite} />
+      </S.UserGroup>
+    </S.HomeWrapper>
+  );
 }
 
 export default Home;
