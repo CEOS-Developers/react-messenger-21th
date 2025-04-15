ec0f362b5257e1f2a4ec94e391ea735f8691a6b7
2025314242 <donguk.kwon@yonsei.ac.kr>
2025-04-15 21:24:00 +0900

    fix: error

--- src/utils/searchFromStorage.ts
+++ src/utils/searchFromStorage.ts
@@ -1,6 +1,8 @@
 import Hangul from 'hangul-js';
 
 function searchFromStorage(searchValue: string, corpus: string[]): number[] {
+  if (!searchValue) return [];
+
   return corpus.reduce<number[]>((acc, text, idx) => {
     if (Hangul.search(text.toLowerCase(), searchValue.toLowerCase()) > -1) {
       acc.push(idx);
