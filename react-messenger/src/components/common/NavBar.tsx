b16783f8bd44c2c270299119e243eb713ec3b90b
chaeyoungwon <epopcy@naver.com>
2025-04-15 22:28:01 +0900

    Fix: clsx 추가 및 NavBar 높이 고정

--- react-messenger/src/components/common/NavBar.tsx
+++ react-messenger/src/components/common/NavBar.tsx
@@ -1,4 +1,6 @@
 import { NavLink } from 'react-router-dom';
+import clsx from 'clsx';
+
 import HomeOn from '@/assets/svgs/home/HomeOn.svg?url';
 import HomeOff from '@/assets/svgs/home/HomeOff.svg?url';
 import ChatOn from '@/assets/svgs/home/ChatOn.svg?url';
@@ -29,21 +31,20 @@ const menus = [
 
 const NavBar = () => {
   return (
-    <div className="w-[375px] h-[86px] sticky bottom-0 shadow-default bg-grey-50">
-      <div className="flex justify-center w-full pt-3 px-4 pb-6 h-auto gap-[84px]">
+    <div className="w-[375px] h-[86px] flex shrink-0 sticky bottom-0 shadow-default bg-grey-50">
+      <div className="flex justify-center w-full pt-3 px-4 pb-6 h-full gap-[84px]">
         {menus.map((menu) => (
-          <NavLink
-            key={menu.name}
-            to={menu.path}
-            className={({ isActive }) =>
-              `flex flex-col items-center justify-center gap-[3px] ${isActive ? 'text-grey-900' : 'text-grey-400'}`
-            }
-          >
+          <NavLink key={menu.name} to={menu.path}>
             {({ isActive }) => (
-              <>
+              <div
+                className={clsx(
+                  'flex flex-col items-center justify-center gap-[3px]',
+                  isActive ? 'text-grey-900' : 'text-grey-400',
+                )}
+              >
                 <img src={isActive ? menu.iconOn : menu.iconOff} alt={menu.name} className="w-[24px] h-[24px]" />
-                <span className="caption-2 font-medium">{menu.name}</span>
-              </>
+                <span className="caption-2">{menu.name}</span>
+              </div>
             )}
           </NavLink>
         ))}
