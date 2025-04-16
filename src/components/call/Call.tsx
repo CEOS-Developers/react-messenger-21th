ba6f22eb51b6c9abc619263be85400e648bf8f4c
2025314242 <donguk.kwon@yonsei.ac.kr>
2025-04-16 14:40:10 +0900

    test: testing...

--- src/components/call/Call.tsx
+++ src/components/call/Call.tsx
@@ -1,5 +1,5 @@
 function Call() {
-  return <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 loader" />;
+  return <div className="absolute top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2 loader" />;
 }
 
 export default Call;
