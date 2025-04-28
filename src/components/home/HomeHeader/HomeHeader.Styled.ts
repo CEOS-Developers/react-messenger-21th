25f5e7dee5f48153f9a7759b9beafae8be9e71a4
2025314242 <donguk.kwon@yonsei.ac.kr>
2025-04-28 16:46:08 +0900

    fix: minor errors
    - typing error
    - empty file

--- src/components/home/HomeHeader/HomeHeader.Styled.ts
+++ src/components/home/HomeHeader/HomeHeader.Styled.ts
@@ -4,7 +4,7 @@ const HomeHeaderWrapper = styled.header`
   position: absolute;
   top: var(--statusbar-height);
   width: 100%;
-  heigth: 46px;
+  height: 46px;
   padding: 11px var(--phone-margin);
   gap: 8px;
   display: flex;
